ce98e750054c88a44a010b16d65034194e17b66d
Saksham-Shukla-05 <get467997@gmail.com>
2025-09-04 00:06:36 +0530

    refactor : the route at jobsRoute

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -3,7 +3,7 @@ import { getJobs, createJob } from "../controllers/jobsController.js";
 
 const router = express.Router();
 
-router.get("/jobs", getJobs);
-router.post("/jobs/create", createJob);
+router.get("/", getJobs);
+router.post("/create", createJob);
 
 export default router;
