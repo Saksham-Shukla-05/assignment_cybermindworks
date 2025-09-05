f1546d03582dd73fb79e378335503a7b0d59d29f
Saksham-Shukla-05 <get467997@gmail.com>
2025-09-05 15:27:11 +0530

    fix : cors issues

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://assignment-cybermindworks.vercel.app/",
     methods: ["GET", "POST"],
   })
 );
